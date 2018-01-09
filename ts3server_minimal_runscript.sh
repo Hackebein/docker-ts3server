0708c0b669a0e42028ef8ce8937eb13f7f4f9465
Lars Olzem <hackebein@gmail.com>
2018-01-09 15:55:42 +0000

    Initial add Basic files

---
+++ ts3server_minimal_runscript.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+export LD_LIBRARY_PATH=".:$LD_LIBRARY_PATH"
+
+D1=$(readlink -f "$0")
+D2=$(dirname "${D1}")
+cd "${D2}"
+if [ -z "${TS3_QUERY_PASSWORD}" ]; then
+	exec ./ts3server $@
+else
+	exec ./ts3server serveradmin_password=${TS3_QUERY_PASSWORD} $@
+fi
+
