42e30d9d1a12a67bab0f0026c6a338d6fb87fe88
Lars Olzem <hackebein@gmail.com>
2018-06-14 21:10:33 +0100

    add patch disable batches

--- ts3server_minimal_runscript.sh
+++ ts3server_minimal_runscript.sh
@@ -5,6 +5,11 @@ export LD_LIBRARY_PATH=".:./redist:$LD_LIBRARY_PATH"
 D1=$(readlink -f "$0")
 D2=$(dirname "${D1}")
 cd "${D2}"
+
+if [ -z "${TS3_PATCH_BADGES_DISABLE}" ]; then
+	sed -e 's/\o0client_badges\o0/\o0client_badxxx\o0/g' -i ts3server
+fi
+
 if [ -z "${TS3_QUERY_PASSWORD}" ]; then
 	exec ./ts3server $@
 else
