fae50857c2a03d8d92c7b5f61ba1f5aecf47b8a1
Lars Olzem <hackebein@gmail.com>
2018-06-15 00:22:48 +0100

    complete patch badge disable

--- ts3server_minimal_runscript.sh
+++ ts3server_minimal_runscript.sh
@@ -6,12 +6,22 @@ D1=$(readlink -f "$0")
 D2=$(dirname "${D1}")
 cd "${D2}"
 
-if [ "${TS3_PATCH_BADGES_DISABLE}" -eq "true" ]; then
-	sed -e 's/\o0client_badges\o0/\o0client_badxxx\o0/g' -i ts3server
+# patching
+if [ -e ts3server.dist ]; then
+	cp -a ts3server.dist ts3server
+	rm ts3server.dist
+fi
+if [ "${TS3SERVER_PATCH_ENABLE}" = "true" ]; then
+	cp -a ts3server ts3server.dist
+	# Patch for disable badges
+	if [ "${TS3SERVER_PATCH_BADGES_DISABLE}" = "true" ]; then
+		sed -e 's/client_badges/client_BADGES/g' -i ts3server
+	fi
 fi
 
-if [ -z "${TS3_QUERY_PASSWORD}" ]; then
+# execution
+if [ -z "${TS3SERVER_QUERY_PASSWORD}" ]; then
 	exec ./ts3server $@
 else
-	exec ./ts3server serveradmin_password=${TS3_QUERY_PASSWORD} $@
+	exec ./ts3server serveradmin_password=${TS3SERVER_QUERY_PASSWORD} $@
 fi
