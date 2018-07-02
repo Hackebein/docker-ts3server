52af6ee3a9271cf440d50dc50da0122b1309f8fc
Lars Olzem <hackebein@gmail.com>
2018-07-02 16:46:23 +0200

    update release helper

--- update_releases.sh
+++ update_releases.sh
@@ -3,13 +3,12 @@ set -e
 
 cd "$(dirname "$(readlink -f "$0")")"
 
-rm -rf ts3server_*
+rm -rf ts3server_* releases.new
 while read release; do
     version=$(echo ${release} | cut -f1 -d'|' | sed 's/#//g')
     arch=$(echo ${release} | cut -f2 -d'|')
     sha256old=$(echo ${release} | cut -f3 -d'|')
     url=$(echo ${release} | cut -f4 -d'|')
-    wget "$url" -qO "$version"
-    echo "${version}|${arch}|$(sha256sum "$version" | cut -f1 -d' ')|${url}"
-    rm "$version"
+    echo "${version}|${arch}|$(wget "$url" -qO - | sha256sum | cut -f1 -d' ')|${url}" >> releases.new
 done < releases
+mv releases.new releases
